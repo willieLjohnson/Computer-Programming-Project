e9aca66aa895293c486a0fe07333ca97d5b646bb
SlickJohnson <liwa.johnson@gmail.com>
2015-01-26 19:28:40 -0500

    All the files.
    Just uploading the files needed

---
+++ src/ca/johnson/game/entities/PlayerMP.java
@@ -0,0 +1,29 @@
+package ca.johnson.game.entities;
+
+import java.net.InetAddress;
+
+import ca.johnson.game.InputHandler;
+import ca.johnson.game.level.Level;
+
+public class PlayerMP extends Player {
+
+    public InetAddress ipAddress;
+    public int port;
+
+    public PlayerMP(Level level, int x, int y, InputHandler input, String username, InetAddress ipAddress, int port) {
+        super(level, x, y, input, username);
+        this.ipAddress = ipAddress;
+        this.port = port;
+    }
+
+    public PlayerMP(Level level, int x, int y, String username, InetAddress ipAddress, int port) {
+        super(level, x, y, null, username);
+        this.ipAddress = ipAddress;
+        this.port = port;
+    }
+
+    @Override
+    public void tick() {
+        super.tick();
+    }
+}
